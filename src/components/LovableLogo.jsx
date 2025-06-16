4db388247b70e1b59d57a7c7f8eb057a17cf45e4
PixelPadThai <supagit@eigthy.one>
2025-06-16 20:40:02 +0700

    📋 Replace heart icons with clipboard SVG - Perfect thematic match for clipboard app while keeping gradient animations

--- src/components/LovableLogo.jsx
+++ src/components/LovableLogo.jsx
@@ -2,46 +2,49 @@ import React from 'react';
 
 const LovableLogo = ({ className, style }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 23 24" className={className} style={style}>
-      <mask id="lovable-logo_svg__b" width="23" height="24" x="0" y="0" maskUnits="userSpaceOnUse" style={{ maskType: 'alpha' }}>
-        <path fill="url(#lovable-logo_svg__a)" fillRule="evenodd" d="M6.898 0c3.81 0 6.898 3.179 6.898 7.1v2.7h2.295c3.81 0 6.898 3.178 6.898 7.1S19.901 24 16.091 24H0V7.1C0 3.18 3.088 0 6.898 0" clipRule="evenodd"></path>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 32 32" className={className} style={style}>
+      <mask id="clipboard-logo_svg__b" width="32" height="32" x="0" y="0" maskUnits="userSpaceOnUse" style={{ maskType: 'alpha' }}>
+        <g fill="url(#clipboard-logo_svg__a)">
+          <path d="M9.101,7l8.899,0c1.857,-0 3.637,0.737 4.95,2.05c1.313,1.313 2.05,3.093 2.05,4.95l0,8.899c0.953,-0.195 1.837,-0.665 2.536,-1.363c0.937,-0.938 1.464,-2.21 1.464,-3.536c0,-2.977 0,-7.023 0,-10c0,-1.326 -0.527,-2.598 -1.464,-3.536c-0.938,-0.937 -2.21,-1.464 -3.536,-1.464c-2.977,0 -7.023,0 -10,0c-1.326,-0 -2.598,0.527 -3.536,1.464c-0.698,0.699 -1.168,1.583 -1.363,2.536Z"/>
+          <path d="M23,14c0,-1.326 -0.527,-2.598 -1.464,-3.536c-0.938,-0.937 -2.21,-1.464 -3.536,-1.464c-2.977,0 -7.023,0 -10,0c-1.326,-0 -2.598,0.527 -3.536,1.464c-0.937,0.938 -1.464,2.21 -1.464,3.536c0,2.977 0,7.023 0,10c-0,1.326 0.527,2.598 1.464,3.536c0.938,0.937 2.21,1.464 3.536,1.464c2.977,-0 7.023,-0 10,-0c1.326,0 2.598,-0.527 3.536,-1.464c0.937,-0.938 1.464,-2.21 1.464,-3.536l0,-10Zm-15,10l10,0c0.552,0 1,-0.448 1,-1c0,-0.552 -0.448,-1 -1,-1l-10,0c-0.552,0 -1,0.448 -1,1c0,0.552 0.448,1 1,1Zm0,-4l10,0c0.552,0 1,-0.448 1,-1c0,-0.552 -0.448,-1 -1,-1l-10,0c-0.552,0 -1,0.448 -1,1c0,0.552 0.448,1 1,1Zm0,-4l10,0c0.552,0 1,-0.448 1,-1c0,-0.552 -0.448,-1 -1,-1l-10,0c-0.552,0 -1,0.448 -1,1c0,0.552 0.448,1 1,1Z"/>
+        </g>
       </mask>
-      <g mask="url(#lovable-logo_svg__b)">
-        <g filter="url(#lovable-logo_svg__c)">
-          <ellipse cx="10.084" cy="12.811" fill="#4B73FF" rx="15.562" ry="15.977"></ellipse>
+      <g mask="url(#clipboard-logo_svg__b)">
+        <g filter="url(#clipboard-logo_svg__c)">
+          <ellipse cx="14.084" cy="16.811" fill="#4B73FF" rx="15.562" ry="15.977"></ellipse>
         </g>
-        <g filter="url(#lovable-logo_svg__d)">
-          <ellipse cx="11.794" cy="4.043" fill="#FF66F4" rx="19.931" ry="15.977"></ellipse>
+        <g filter="url(#clipboard-logo_svg__d)">
+          <ellipse cx="15.794" cy="8.043" fill="#FF66F4" rx="19.931" ry="15.977"></ellipse>
         </g>
-        <g filter="url(#lovable-logo_svg__e)">
-          <ellipse cx="15.045" cy="1.037" fill="#FF0105" rx="15.562" ry="14.031"></ellipse>
+        <g filter="url(#clipboard-logo_svg__e)">
+          <ellipse cx="19.045" cy="5.037" fill="#FF0105" rx="15.562" ry="14.031"></ellipse>
         </g>
-        <g filter="url(#lovable-logo_svg__f)">
-          <ellipse cx="12.071" cy="4.039" fill="#FE7B02" rx="9.359" ry="9.608"></ellipse>
+        <g filter="url(#clipboard-logo_svg__f)">
+          <ellipse cx="16.071" cy="8.039" fill="#FE7B02" rx="9.359" ry="9.608"></ellipse>
         </g>
       </g>
       <defs>
-        <filter id="lovable-logo_svg__c" width="45.444" height="46.274" x="-12.638" y="-10.326" colorInterpolationFilters="sRGB" filterUnits="userSpaceOnUse">
+        <filter id="clipboard-logo_svg__c" width="45.444" height="46.274" x="-8.638" y="-6.326" colorInterpolationFilters="sRGB" filterUnits="userSpaceOnUse">
           <feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
           <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape"></feBlend>
           <feGaussianBlur result="effect1_foregroundBlur_19703_3420" stdDeviation="3.58"></feGaussianBlur>
         </filter>
-        <filter id="lovable-logo_svg__d" width="54.181" height="46.274" x="-15.297" y="-19.094" colorInterpolationFilters="sRGB" filterUnits="userSpaceOnUse">
+        <filter id="clipboard-logo_svg__d" width="54.181" height="46.274" x="-11.297" y="-15.094" colorInterpolationFilters="sRGB" filterUnits="userSpaceOnUse">
           <feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
           <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape"></feBlend>
           <feGaussianBlur result="effect1_foregroundBlur_19703_3420" stdDeviation="3.58"></feGaussianBlur>
         </filter>
-        <filter id="lovable-logo_svg__e" width="45.444" height="42.383" x="-7.677" y="-20.154" colorInterpolationFilters="sRGB" filterUnits="userSpaceOnUse">
+        <filter id="clipboard-logo_svg__e" width="45.444" height="42.383" x="-3.677" y="-16.154" colorInterpolationFilters="sRGB" filterUnits="userSpaceOnUse">
           <feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
           <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape"></feBlend>
           <feGaussianBlur result="effect1_foregroundBlur_19703_3420" stdDeviation="3.58"></feGaussianBlur>
         </filter>
-        <filter id="lovable-logo_svg__f" width="33.038" height="33.538" x="-4.448" y="-12.73" colorInterpolationFilters="sRGB" filterUnits="userSpaceOnUse">
+        <filter id="clipboard-logo_svg__f" width="33.038" height="33.538" x="-0.448" y="-8.73" colorInterpolationFilters="sRGB" filterUnits="userSpaceOnUse">
           <feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
           <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape"></feBlend>
           <feGaussianBlur result="effect1_foregroundBlur_19703_3420" stdDeviation="3.58"></feGaussianBlur>
         </filter>
-        <linearGradient id="lovable-logo_svg__a" x1="7.736" x2="15.072" y1="4.218" y2="23.867" gradientUnits="userSpaceOnUse">
+        <linearGradient id="clipboard-logo_svg__a" x1="11.736" x2="19.072" y1="8.218" y2="27.867" gradientUnits="userSpaceOnUse">
           <stop offset="0.025" stopColor="#FF8E63"></stop>
           <stop offset="0.56" stopColor="#FF7EB0"></stop>
           <stop offset="0.95" stopColor="#4B73FF"></stop>
